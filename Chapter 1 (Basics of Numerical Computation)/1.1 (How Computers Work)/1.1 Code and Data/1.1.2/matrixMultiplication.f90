46b7820cb2712e659438f51ba87f0f3a7643b22b
Zachariah Pence <pence.57@outlook.com>
2024-05-05 22:55:34 -0400

    Added Fortran code for 1.1.2

---
+++ Chapter 1 (Basics of Numerical Computation)/1.1 (How Computers Work)/1.1 Code and Data/1.1.2/matrixMultiplication.f90
@@ -0,0 +1,33 @@
+program matrixMultiplication
+    implicit none
+    integer :: i,j,k,m,n,p
+    real, allocatable, dimension(:,:) :: A,B,C 
+
+    m = 3
+    n = 3
+    p = 3
+    allocate(A(m,n),B(n,p),C(m,p))
+
+    A = transpose(reshape((/1, 2, 3, 4,5,6,7,8,9/), (/m,n/)))
+    B = transpose(reshape((/1, 2, 3, 4,5,6,7,8,9/), (/n,p/)))
+
+    do i = 1,m
+        do j = 1,p 
+            C(i,j)=0
+            do k = 1, min(m,p)
+                C(i,j) = C(i,j) + A(i,k)*B(k,j)
+            end do
+        end do
+    end do
+    deallocate(A,B)
+
+
+    ! Print output in a "matrix-like" format
+    do i = 1,size(C(:,1))
+        do j = 1,size(C(i,:))
+            print "(f40.2,$)", C(i,j)
+        end do
+        print *, ""
+    end do
+    deallocate(C)
+end program matrixMultiplication
